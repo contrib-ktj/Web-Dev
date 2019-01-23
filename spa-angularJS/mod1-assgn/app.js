2355d1747dc3f0d798453c152c6ff64fc166c54c
KTJ <kshtjkmr10@gmail.comgit config --global user.name KTJq>
2019-01-24 01:08:09 +0530

    Assgn1: Lunch Checker Optional Exercises

--- spa-angularJS/mod1-assgn/app.js
+++ spa-angularJS/mod1-assgn/app.js
@@ -7,9 +7,14 @@
   function LunchCheckController($scope){
     $scope.inp="";
     $scope.msg="";
+    $scope.msg_col="";
+    $scope.border_col="";
     $scope.DisplayMsg=function () {
-      if($scope.inp==='')
+      if($scope.inp===''){
         $scope.msg="Please enter data first";
+        $scope.msg_col="Red"
+        $scope.border_col="Red";
+      }
       else {
         var words=$scope.inp.split(',');
         var count=0;
@@ -18,12 +23,21 @@
             continue;
           else
             count++;
-        if(count===0)
+        if(count===0){
           $scope.msg="Please enter data first";
-        else if (count<=3)
+          $scope.msg_col="Red"
+          $scope.border_col="Red";
+        }
+        else if (count<=3){
           $scope.msg="Enjoy!";
-        else
+          $scope.msg_col="Green"
+          $scope.border_col="Green";
+        }
+        else{
           $scope.msg="Too much!";
+          $scope.msg_col="Green"
+          $scope.border_col="Green";
+        }
       }
     }
   }
